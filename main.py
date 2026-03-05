e785edff15c55c2631901dc99b4f0c9377917de4
emirmontiel7-debug <emirmontiel7@gmail.com>
2026-03-04 22:22:52 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from modelos import *
 
-print("--- REGISTRO MANUAL DE INVENTARIO (10 PELÍCULAS) ---")
+print("--- REGISTRO MANUAL DE INVENTARIO ---")
 
+# --- 10 OBJETOS DE LA CLASE PELICULA ---
 p1 = Pelicula("Titanic", 194, "B", "Romance")
 p2 = Pelicula("Shrek", 90, "AA", "Animación")
 p3 = Pelicula("El Rey León", 88, "AA", "Animación")
@@ -13,36 +14,51 @@ p8 = Pelicula("Coco", 105, "AA", "Animación")
 p9 = Pelicula("Batman", 152, "B15", "Acción")
 p10 = Pelicula("Rápido y Furioso", 106, "B", "Acción")
 
-print(f"ID: 1  | {p1.mostrar_detalle()}")
-print(f"ID: 2  | {p2.mostrar_detalle()}")
-print(f"ID: 3  | {p3.mostrar_detalle()}")
-print(f"ID: 4  | {p4.mostrar_detalle()}")
-print(f"ID: 5  | {p5.mostrar_detalle()}")
-print(f"ID: 6  | {p6.mostrar_detalle()}")
-print(f"ID: 7  | {p7.mostrar_detalle()}")
-print(f"ID: 8  | {p8.mostrar_detalle()}")
-print(f"ID: 9  | {p9.mostrar_detalle()}")
-print(f"ID: 10 | {p10.mostrar_detalle()}")
+print(f"PELÍCULA 1: {p1.mostrar_detalle()}")
+print(f"PELÍCULA 2: {p2.mostrar_detalle()}")
+print(f"PELÍCULA 3: {p3.mostrar_detalle()}")
+print(f"PELÍCULA 4: {p4.mostrar_detalle()}")
+print(f"PELÍCULA 5: {p5.mostrar_detalle()}")
+print(f"PELÍCULA 6: {p6.mostrar_detalle()}")
+print(f"PELÍCULA 7: {p7.mostrar_detalle()}")
+print(f"PELÍCULA 8: {p8.mostrar_detalle()}")
+print(f"PELÍCULA 9: {p9.mostrar_detalle()}")
+print(f"PELÍCULA 10: {p10.mostrar_detalle()}")
 
-print("--- VALIDACIÓN DE DATOS FINALIZADA ---\n")
+print("\n--- REGISTRO DE USUARIOS ---")
+u1 = Usuario(101, "Ana", "ana@mail.com", "555-01", 100)
+u2 = Usuario(102, "Beto", "beto@mail.com", "555-02", 150)
+u3 = Usuario(103, "Carla", "carla@mail.com", "555-03", 200)
+u4 = Usuario(104, "Dani", "dani@mail.com", "555-04", 50)
+u5 = Usuario(105, "Emir", "emir@mail.com", "555-05", 300)
+u6 = Usuario(106, "Fer", "fer@mail.com", "555-06", 120)
+u7 = Usuario(107, "Gaby", "gaby@mail.com", "555-07", 80)
+u8 = Usuario(108, "Hugo", "hugo@mail.com", "555-08", 400)
+u9 = Usuario(109, "Irma", "irma@mail.com", "555-09", 110)
+u10 = Usuario(110, "Juan", "juan@mail.com", "555-10", 90)
 
-print("se inicia el proceso")
-user = Usuario(88, "Carlos88", "c@mail.com", "555-1234", 150)
+print(f"USUARIO 1: {u1.nombre} | Puntos: {u1.puntosFidelidad}")
+print(f"USUARIO 5: {u5.nombre} | Puntos: {u5.puntosFidelidad} (Usuario VIP)")
+print(f"USUARIO 10: {u10.nombre} | Puntos: {u10.puntosFidelidad}")
+
+print("Procesos de Reserva")
+user_test=u5
 sala04 = Sala(4, "04", "Planta Baja", "IMAX", 100)
-funcion1 = Funcion(p5, sala04, "20:00", 150)
+funcion1 = Funcion(p5, sala04, "20:00", 150) # Avatar
 
-print(f"Usuario: {user.nombre} (Puntos: {user.puntosFidelidad})")
-print(f"Película: '{funcion1.pelicula.titulo}' | Sala: {funcion1.sala.nombre} ({funcion1.sala.tipo})")
+print(f"Usuario activo: {user_test.nombre}")
+print(f"Película seleccionada: '{funcion1.pelicula.titulo}'")
+
 
 print("Seleccione sus asientos: A1, A2, B5")
 print("El asiento A2 ya está ocupado por la Reserva #882.")
 print("seleccione asientos disponibles.")
 
 print("\nSeleccione sus asientos: A1, A3, B5")
-print("Asientos A1, A3, B5 bloqueados con exito.")
-print("Monto base: $450.00")
+print("[ Asientos A1, A3, B5 bloqueados con éxito.")
+print("Monto total a pagar: $450.00")
 
-print("Metodos indivisduales")
-admin=Empleado(10, "Admin_Juan", "a@cine.com", "342423", "ADMIN", "Matutino")
-print(f"Prueba gestionarFunciones: {admin.gestionarFunciones()}")
-print(f"Prueba limpiarEspacio: {sala04.limpiarEspacio()}")
+print("Metodos ")
+admin = Empleado(10, "Admin_Juan", "a@cine.com", "342423", "ADMIN", "Matutino")
+print(f"Acción Empleado: {admin.gestionarFunciones()}")
+print(f"Acción Sala: {sala04.limpiarEspacio()}")
